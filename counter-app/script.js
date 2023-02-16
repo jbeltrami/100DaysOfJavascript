ea35cae19d7829f19c081f1d40220a5eeb3ad546
jBeltrami <joaopbeltrami@gmail.com>
2023-02-16 12:54:41 -0300

    Update count font color on event.

--- counter-app/script.js
+++ counter-app/script.js
@@ -3,10 +3,17 @@ const buttons = document.querySelector(".buttons");
 
 buttons.addEventListener("click", (e) => {
   return e.target.classList.contains("add")
-    ? count.innerHTML++
+    ? (count.innerHTML++, setColor())
     : e.target.classList.contains("subtract")
-    ? count.innerHTML--
+    ? (count.innerHTML--, setColor())
     : e.target.classList.contains("reset")
-    ? (count.innerHTML = 0)
+    ? ((count.innerHTML = 0), setColor())
     : null;
 });
+
+function setColor() {
+  if (count.innerHTML > 0) return (count.style.color = "yellow");
+  if (count.innerHTML < 0) return (count.style.color = "orangered");
+
+  return (count.style.color = "white");
+}
